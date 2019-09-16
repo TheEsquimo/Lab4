626903e596027f6b74e1ccab34cea10dfb44ab98
Elvis <elvostudios@hotmail.com>
2019-09-16 20:29:38 +0200

    Added array loop to map generation method

--- Lab4/LevelMap.cs
+++ Lab4/LevelMap.cs
@@ -18,6 +18,36 @@
             int mapSizeX = charMap.GetLength(0);
             int mapSizeY = charMap.GetLength(1);
             levelMap = new MapTile[mapSizeX, mapSizeY];
+
+            for (int row = 0; row < mapSizeY; row++)
+            {
+                for (int column = 0; column < mapSizeX; column++)
+                {
+                    //Switch that makes objects based on characters
+                    switch (charMap[row, column])
+                    {
+                        case '#':
+
+                            break;
+
+                        case '.':
+
+                            break;
+
+                        case 'k':
+
+                            break;
+
+                        case 'm':
+
+                            break;
+
+                        default:
+
+                            break;
+                    }
+                }
+            }
         }
     }
 }
